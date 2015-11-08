e3af2c5989173462a2212d14d25c295bc4e47eb9
Dmitry Smal <mialinx@gmail.com>
2015-11-08 19:26:48 +0300

    more

--- test_l11.py
+++ test_l11.py
@@ -6,8 +6,8 @@ import os
 
 home = '/home/box'
 
-def proxy(s, cmd):
-    cmd = 'PYTHONPATH=/home/box/web/ask/ python2.7 ' + cmd + ' 2>&1'
+def proxy(s, tst):
+    cmd = 'PYTHONPATH={0}/web/ask/ python2.7 {0}/{1} 2>&1'.format(home, tst)
     res = s.run(cmd)
     if res.succeeded:
         msgs = re.findall('^(?!ERROR|FAIL)^\w+:\s*(.*)\s*$', res, flags=re.M)
@@ -17,13 +17,78 @@ def proxy(s, cmd):
         raise Exception(res)
 
 def test_models(s):
-    proxy('test_l11_server.py TestModels')
+    proxy(s, 'test_l11_server.py TestModels')
 
 def test_profile(s):
-    proxy('test_l11_server.py TestProfile')
+    proxy(s, 'test_l11_server.py TestProfile')
 
 def test_question(s):
-    proxy('test_l11_server.py TestQuestion')
+    proxy(s, 'test_l11_server.py TestQuestion')
 
 def test_answer(s):
-    proxy('test_l11_server.py TestAnswer')
+    proxy(s, 'test_l11_server.py TestAnswer')
+
+'''
+mkdir -p /home/box/web/public
+mkdir -p /home/box/web/public/js
+mkdir -p /home/box/web/public/css
+mkdir -p /home/box/web/public/img
+mkdir -p /home/box/web/uploads/
+mkdir -p /home/box/web/etc/
+cat > /home/box/web/etc/nginx.conf <<EOC
+server {
+    listen 80 default_server;
+    location / {
+        proxy_pass http://127.0.0.1:8080/;
+    }
+    location ~ \.\w\w\w?\w?$ {
+        root /home/box/web/public/;
+    }
+    location ^~ /uploads/ {
+        alias /home/box/web/uploads/;
+    }
+}
+EOC
+sudo unlink /etc/nginx/sites-enabled/default
+sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+sudo /usr/sbin/nginx -c /etc/nginx/nginx.conf
+sudo /etc/init.d/nginx restart
+
+(cd /home/box/web && django-admin startproject ask)
+(cd /home/box/web/ask && python2.7 manage.py startapp qa)
+
+cat > /home/box/web/ask/ask/urls.py <<EOC
+from django.conf.urls import patterns, include, url
+from qa.views import test
+urlpatterns = patterns('',
+    url(r'^$', test, name='home'),
+    url(r'login/$', test, name='login'),
+    url(r'signup/$', test, name='signup'),
+    url(r'ask/$', test, name='ask'),
+    url(r'popular/$', test, name='popular'),
+    url(r'new/$', test, name='new'),
+    url(r'question/(?P<id>\d+)/$', test, name='question'),
+)
+EOC
+
+cat > /home/box/web/ask/qa/views.py <<EOC
+from django.shortcuts import render
+from django.http import HttpResponse
+def test(request, *args, **kwargs):
+    return HttpResponse('OK')
+EOC
+
+cat > /home/box/web/etc/django <<EOC
+CONFIG = {
+    'working_dir': '/home/box/web/ask',
+    'args': (
+        '--bind=0.0.0.0:8080',
+        '--workers=3',
+        '--timeout=60',
+        'ask.wsgi:application',
+    ),
+}
+EOC
+sudo ln -s /home/box/web/etc/django /etc/gunicorn.d/django
+sudo /etc/init.d/gunicorn restart
+'''
--- test_l11_server.py
+++ test_l11_server.py
@@ -1,12 +1,14 @@
 import os
-os.environ['DJANGO_SETTINGS_MODULE'] = 'ask.settings'
-from django.db.models.fields import FieldDoesNotExist
-from django.db.models.fields import CharField, TextField, IntegerField
-from django.db.models.fields.files import ImageField
-from django.db.models.fields.related import ForeignKey, ManyToManyField
-
 import unittest
 import sys
+sys.path.append('/home/box/web/ask')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'ask.settings'
+from django.db.models.fields import FieldDoesNotExist
+from django.db.models.fields import CharField, TextField, IntegerField, DateField
+from django.db.models.fields.files import ImageField
+from django.db.models.fields.related import ForeignKey, ManyToManyField
+from django.contrib.auth.models import User
+
 
 
 class TestModels(unittest.TestCase):
@@ -52,12 +54,29 @@ class TestQuestion(unittest.TestCase):
         except FieldDoesNotExist:
             assert False, "author field does not exist in Question model"
         assert isinstance(author, ForeignKey), "author field is not ForeignKey"
-        assert author.related_field.model == User, "author field does not refer User model"
+        assert author.related.parent_model == User, "author field does not refer User model"
+        try:
+            likes = Question._meta.get_field('likes')
+        except FieldDoesNotExist:
+            assert False, "likes field does not exist in Question model"
+        assert isinstance(likes, ManyToManyField), "likes field is not ManyToManyField"
+        assert likes.related.parent_model == User, "likes field does not refer User model"
+
+
+class TestQuestionManager(unittest.TestCase):
+    def test_question_manager(self):
+        from qa.models import Question
+        from qa.models import QuestionManager
+        mgr = Question.objects
+        assert isinstance(mgr, QuestionManager), "Question.objects is not an QuestionManager"
+        assert hasattr(mgr, 'new'), "QuestionManager has no 'new' queryset method"
+        assert hasattr(mgr, 'hot'), "QuestionManager has no 'hot' queryset method"
 
 
 class TestAnswer(unittest.TestCase):
     def test_answer(self):
         from qa.models import Answer
+        from qa.models import Question
         try:
             text = Answer._meta.get_field('text')
         except FieldDoesNotExist:
@@ -68,7 +87,7 @@ class TestAnswer(unittest.TestCase):
         except FieldDoesNotExist:
             assert False, "question field does not exist in Answer model"
         assert isinstance(question, ForeignKey), "question field is not ForeignKey"
-        assert author.related_field.model == Question, "question field does not refer Question model"
+        assert question.related.parent_model == Question, "question field does not refer Question model"
         try:
             added_at = Answer._meta.get_field('added_at')
         except FieldDoesNotExist:
@@ -79,7 +98,7 @@ class TestAnswer(unittest.TestCase):
         except FieldDoesNotExist:
             assert False, "author field does not exist in Answer model"
         assert isinstance(author, ForeignKey), "author field is not ForeignKey"
-        assert author.related_field.model == User, "author field does not refer User model"
+        assert author.related.parent_model == User, "author field does not refer User model"
 
 
 suite = unittest.TestLoader().loadTestsFromTestCase(globals().get(sys.argv[1]))
