b3d9ed3a79131ff8f9e8b3b3c5e9a5819e9b1480
Dmitry Smal <mialinx@gmail.com>
2015-11-13 00:47:57 +0300

    more

--- solution.sh
+++ solution.sh
@@ -152,6 +152,11 @@ from django import forms
 class AskForm(forms.Form):
     title = forms.CharField(max_length=100)
     text = forms.CharField(widget=forms.Textarea)
+
+class AnswerForm(forms.Form):
+    text = forms.CharField()
+    question = forms.IntegerField()
+
 EOC
 
 # views
